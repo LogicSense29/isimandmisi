c5510316cb866d0fc8ae0eec926ef818604de402
Odunsi Oluwabukola <meetbjfunk@gmail.com>
2024-01-02 23:56:07 +0100

    second commit

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -8,7 +8,13 @@ function Header() {
                     <NavBar/>
                     <div className="hero-box">
                         <div className="hero-box-item-one"></div>
-                        <div className="hero-box-item-two"></div>
+                        <div className="hero-box-item-two">
+                        <div className="hero-text">
+                        <h1 className="hero-heading">A Place where Your health Resonants</h1>
+                        <p className="hero-p"></p>
+                        <button id="cta">BOOK A SESSION</button>
+                        </div>
+                        </div>
                         <div className="hero-box-item-three"></div>
                     </div>
                 </header>
